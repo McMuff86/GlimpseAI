12b35af88e3a3f72f8b6da609c6b3558951a7d71
Sentinel <sentinel.core.ai@gmail.com>
2026-02-08 01:17:52 +0100

    init: GlimpseAI project scaffold - Rhino 8 plugin for AI preview rendering

---
+++ src/GlimpseAI/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+[assembly: AssemblyTitle("Glimpse AI")]
+[assembly: AssemblyDescription("Real-time AI Preview Rendering for Rhino 8")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Muff Software")]
+[assembly: AssemblyProduct("GlimpseAI")]
+[assembly: AssemblyCopyright("Copyright © 2026 Adrian Muff")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A3F7B2E1-9C84-4D6F-B5E3-1A2D8F4C6E90")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
+
+[assembly: PlugInDescription(DescriptionType.Address, "")]
+[assembly: PlugInDescription(DescriptionType.Country, "Switzerland")]
+[assembly: PlugInDescription(DescriptionType.Email, "")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Organization, "Muff Software")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "https://github.com/McMuff86/GlimpseAI")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "https://github.com/McMuff86/GlimpseAI")]
